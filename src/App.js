b5cc0055917cf63bafe2d186e565e78ac3c1fd60
connorwarme <connor.warme@gmail.com>
2022-12-18 14:37:04 -0700

    Install react-router-dom, create initial components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }

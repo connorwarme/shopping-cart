0653a400b561345aa35e52a8dbecc366f2690d51
connorwarme <connor.warme@gmail.com>
2022-12-21 19:49:11 -0700

    Add content to About component

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,10 @@ const About = () => {
     return (
         <div className="about">
             <h1>About Amity</h1>
+            <p>I climb full-time. My husband, Connor, and I live out of a van that we built out nearly 5 years ago. We travel to climbing destinations throughout the western US.</p>
+            <p>My favorite climbing zones: Yosemite, CA, and Indian Creek, UT.</p>
+            <p>I graduated with a Master's in Sports Nutrition from the University of Colorado at Colorado Springs in December 2021.</p>
+            <p>I'm excited to combine my </p>
         </div>
     )
 }

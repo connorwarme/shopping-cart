f3836297e6dc551e63910ec3f627d25f57326202
connorwarme <connor.warme@gmail.com>
2022-12-24 13:14:29 -0700

    Add indicator for when package is added to cart on Packages screen; still needed on individual Package pages

--- src/components/ProdCard.js
+++ src/components/ProdCard.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Add from "../imgs/cart-check.png";
 
 const ProdCard = ({ data, add, view }) => {
-    const master = [data, add]
+    const addedDisplay = (dom) => {
+        dom.textContent = '';
+        const photo = document.createElement('img');
+        photo.src = Add;
+        photo.alt = "Added ✔️";
+        dom.appendChild(photo);
+        dom.classList.add('add-check');
+        console.log('did it work?')
+    }
+    const normalDisplay = (dom) => {
+        dom.removeChild(dom.firstChild);
+        dom.textContent = "Add to Cart";
+        dom.classList.remove('add-check');
+    }
+    const addToCartVisual = (e) => {
+        add(e);
+        console.log(e.target);
+        addedDisplay(e.target);
+        setTimeout(() => {normalDisplay(e.target)}, 10000);
+
+    }
     return (
         <div className="product-card" id={data.id}>
             <h1>{data.title}</h1>
             <p>{data.about}</p>
             <div className="product-buttons">
                 <button className="view" id={data.id} onClick={view}><Link to={`/products/${data.url}`} state={data}>View Product</Link></button>
-                <button className="add-to-cart" id={data.id} onClick={add}>Add to Cart</button>
+                <button className="add-to-cart" id={data.id} onClick={addToCartVisual}>Add to Cart</button>
             </div>
         </div>
     )

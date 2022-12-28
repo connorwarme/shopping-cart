0bd124c019c17d5f72636841875e7092ff6420f8
connorwarme <connor.warme@gmail.com>
2022-12-27 17:37:22 -0700

    Work on styling; trying to add photographer credit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,22 @@ import GitHub from "../imgs/github.svg";
 import LinkedIn from "../imgs/linkedin.svg";
 import "../style/Footer.css";
 
-const Footer = () => {
+const Footer = ({data}) => {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+    // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+    let value = params.some_key; // "some_value"
+    console.log(value);
+    const action = () => {
+        console.log(params);
+        console.log(value);
+    }
+
     return (
         <div className="footer-container"> 
             <div className="footer-content">
+                <p>Photographer: {action()}</p>
                 <div className="icon-container">
                     <a href="https://github.com/connorwarme">
                         <img src={GitHub} alt="GitHub Link" />
